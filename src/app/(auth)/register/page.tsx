934bed22a39b295cfc151ebedf80d465b8c90796
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-12-09 22:16:50 +0530

    Navbar styled for form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -19,7 +19,7 @@ export default function RegisterPage() {
 
   return (
     <main>
-      <Navbar />
+      <Navbar className="bg-primary" />
       <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="flex w-full max-w-sm flex-col gap-6">
           <RegisterForm />
